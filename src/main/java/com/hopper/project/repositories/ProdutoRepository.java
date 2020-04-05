2126af74ff015a26ce3aa5dd6f4f988f263384f5
Anderson Kistner <andersonkistner@hotmail.com>
2020-04-05 15:33:19 -0300

    Classe Produto e associação muitos para muitos criados

--- src/main/java/com/hopper/project/repositories/ProdutoRepository.java
+++ src/main/java/com/hopper/project/repositories/ProdutoRepository.java
@@ -3,8 +3,8 @@ package com.hopper.project.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.hopper.project.domain.Categoria;
+import com.hopper.project.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 }
