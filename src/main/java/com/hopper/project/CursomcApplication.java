e294392c301b08984241d3aad97fc3f88b67a7cd
Anderson Kistner <andersonkistner@hotmail.com>
2020-04-09 19:10:47 -0300

    Classes Cliente, TipoCliente e Endereco criadas

--- src/main/java/com/hopper/project/CursomcApplication.java
+++ src/main/java/com/hopper/project/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.hopper.project.domain.Categoria;
 import com.hopper.project.domain.Cidade;
+import com.hopper.project.domain.Cliente;
+import com.hopper.project.domain.Endereco;
 import com.hopper.project.domain.Estado;
 import com.hopper.project.domain.Produto;
+import com.hopper.project.domain.enums.TipoCliente;
 import com.hopper.project.repositories.CategoriaRepository;
 import com.hopper.project.repositories.CidadeRepository;
+import com.hopper.project.repositories.ClienteRepository;
+import com.hopper.project.repositories.EnderecoRepository;
 import com.hopper.project.repositories.EstadoRepository;
 import com.hopper.project.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository EnderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -49,6 +58,11 @@ public class CursomcApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+				
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto. 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
@@ -59,10 +73,15 @@ public class CursomcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "9383893"));
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		EnderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
