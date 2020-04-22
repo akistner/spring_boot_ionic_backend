cdf5f268256ecc927d6482ba7c6b9913ecc4ff50
Anderson Kistner <andersonkistner@hotmail.com>
2020-04-22 15:24:46 -0300

    Endpoint /clientes/{id} ajustado

---
+++ src/main/java/com/hopper/project/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.hopper.project.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hopper.project.domain.Cliente;
+import com.hopper.project.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/hopper/project/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.hopper.project.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hopper.project.domain.Cliente;
+import com.hopper.project.repositories.ClienteRepository;
+import com.hopper.project.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado. Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
