255f9fbf2794bf6f09ea0399d32255bb3400236d
Anderson Kistner <andersonkistner@hotmail.com>
2020-05-26 14:42:41 -0300

    Validação customizada (Email) para POST em Cliente criada

--- src/main/java/com/hopper/project/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/hopper/project/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hopper.project.domain.Cliente;
 import com.hopper.project.domain.enums.TipoCliente;
 import com.hopper.project.dto.ClienteNewDTO;
+import com.hopper.project.repositories.ClienteRepository;
 import com.hopper.project.resources.exceptions.FieldMessage;
 import com.hopper.project.services.validation.utils.Document;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !Document.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repository.findByEmail(objDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
