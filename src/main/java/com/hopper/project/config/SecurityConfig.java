38bdf6d6c2d7ac4c720e54e2c92f5325b1246b5e
Anderson Kistner <andersonkistner@hotmail.com>
2020-06-10 16:26:34 -0300

    Autorização implementada

--- src/main/java/com/hopper/project/config/SecurityConfig.java
+++ src/main/java/com/hopper/project/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.hopper.project.security.JWTAuthenticationFilter;
+import com.hopper.project.security.JWTAuthorizationFilter;
 import com.hopper.project.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
