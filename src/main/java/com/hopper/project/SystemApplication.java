807e888e846ec23f245772df111a40efec5e48c9
Anderson Kistner <andersonkistner@hotmail.com>
2020-06-15 19:07:33 -0300

    Armazenamento no S3 implementado

--- src/main/java/com/hopper/project/SystemApplication.java
+++ src/main/java/com/hopper/project/SystemApplication.java
@@ -1,11 +1,17 @@
 package com.hopper.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hopper.project.services.S3Service;
+
 @SpringBootApplication
 public class SystemApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SystemApplication.class, args);
@@ -13,6 +19,7 @@ public class SystemApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\Oneiros\\Downloads\\Dagon.jpg");
 	}
 	
 }
